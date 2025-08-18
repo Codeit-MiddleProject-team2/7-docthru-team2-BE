89018acf72018f12426e7c163f7be29c090ba8f3
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-08-19 01:19:46 +0900

    최다 좋아요 translation api 추가

--- routes/translation.js
+++ routes/translation.js
@@ -9,6 +9,7 @@ import { verifyTranslationAuth } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+router.get("/best", findAllTranslations);
 router.get("/temporary", findAllTranslations);
 router.get("/best", findBestTranslations);
 // 목록: 특정 challengeId의 번역들 페이지네이션
